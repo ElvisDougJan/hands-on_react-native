9b52b93e2795e1271c74e18384a678f14e323316
Elvis Douglas Janegitz <edjelvis@gmail.com>
2018-12-12 22:34:11 -0200

    Subindo projeto

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Homescreen extends Component {
+  render() {
+    return(
+      <View>
+        <Text>Olá do RN!</Text>
+      </View>
+    )
+  }
+}
